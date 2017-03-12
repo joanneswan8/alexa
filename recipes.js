88bfda14d031f3ed62f79eac23821276834c7cb4
joanneswan8 <joanneswanson@Joannes-MBP.home>
2017-03-12 15:40:35 -0400

    initial load

---
+++ recipes.js
@@ -0,0 +1,41 @@
+/* eslint-disable  func-names */
+/* eslint quote-props: ["error", "consistent"]*/
+module.exports = {
+    RECIPE_EN_US: {
+        
+        'account summary': 'Your credit card balance is $402.32. Your next payment is due on April 15th with a minumum payment of $25. You have $903.28 in your checking account and $3090.22 in your savings account. Looking great!',
+        'summary': 'Your credit card balance is $402.32. Your next payment is due on April 15th with a minumum payment of $25. You have $903.28 in your checking account and $3090.22 in your savings account. Looking great!',
+        
+        'credit card balance': 'Your credit card balance is $402.32',
+        'balance': 'Your credit card balance is $402.32',
+
+        'savings account balance': 'You currently have $3520.10 in your savings account.',
+        'savings account': 'You currently have $3520.10 in your savings account.',
+        'checking account balance': 'You currently have $1011.93 in your checking account.',
+        'checking account': 'You currently have $1011.93 in your checking account.',
+
+        'due date': 'Your credit card payment is due on April 15th.',
+        'transaction': 'Your last transaction with your Evo card was today at Walmart for $28.99.',
+        'purchase': 'Your last transaction with your Evo card was today at Walmart for $28.99.',
+
+        'transfer': 'The transfer will be applied to your account.',
+        'send': 'The transfer will be applied to your account.',
+        'move': 'The transfer will be applied to your account.',
+
+        'spent today': 'You have spent $55.78 on your Evo card today from 3 transactions.',
+        'spent this week': 'You have spent $178.02 on your Evo card this week. That\'s $23.33 less than last week.',
+        'spent this month': 'You have spent $789.11 on your Evo card this week. That\'s $30.22 more than last month.',
+        'spent this year': 'You have spent $5029.12 on your Evo card this week.',
+        'spend today': 'You have spent $55.78 on your Evo card today from 3 transactions.',
+        'spend this week': 'You have spent $178.02 on your Evo card this week. That\'s $23.33 less than last week.',
+        'spend this month': 'You have spent $789.11 on your Evo card this week. That\'s $30.22 more than last month.',
+        'spend this year': 'You have spent $5029.12 on your Evo card this week.',
+
+        'pay balance': 'I have applied your balance payment of $402.32 to your account.',
+        'pay minumum': 'I have applied the minimum payment of $25 to your account.',
+        'pay statement': 'I have applied your statement balance payment of $550.88 to your account.',
+        'pay': 'I have applied the payment to your account.'
+        
+        
+    }
+};
